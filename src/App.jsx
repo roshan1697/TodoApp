46ed2866f2e5fef4bb409eceb6f0bab50264d53a
roshan1697 <roshanpoojary16@gmail.com>
2023-10-26 23:08:15 +0530

    get request completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from './components/navbar'
 import Card from './components/card'
+import axios from 'axios'
 
 function App() {
   const [todos, setTodos] = useState([])
+ 
     // fetch all todos from server
-
+  useEffect(()=>{
+    
+    axios.get('http://localhost:3000/')
+    .then((res)=>{
+      setTodos(res?.data.data)
+      
+     
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+    
+  },[])
   return (
     <>
       <div className='m-6 '>
 
       <Navbar/>
       <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {todos === undefined ?
+          <div className="animate-spin inline-block w-8 h-8 border-[3px] border-current border-t-transparent text-blue-600 rounded-full" role="status" >
+          <span className="sr-only">Loading...</span>
+        </div>
         
-      <Card/>
+        :
+          
+          todos.map((todo, i)=>{
+           return <Card key={i} todo={todo}/>
+            
+          })
+         
+        }
       
       
       </div>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,17 +1,20 @@
 
 
-const Card = () => {
+const Card = (todo) => {
+
   return (
   <div className="flex flex-col gap-2 p-4 m-2.5 overflow-hidden bg-white rounded-lg has-shadow w-80">
   <div className="flex items-baseline justify-between">
-      <h3 className="text-xl font-semibold">Sira team talk</h3>
-      <div className="text-xs">Ricco Xie • 1 June 2023</div>
+      <h3 className="text-lg font-semibold">{todo.todo.title}</h3>
+      <div className="text-xs">Updated {new Date().getDate() - new Date(todo.todo.updatedAt).getDate()} days ago.</div>
   </div>
   <div className="text-sm">
       <p>
-          🕹 Customizable and 😍 accessible design system which provides TailwindCSS
-          component class name library to build modern UI. 💥
+          {todo.todo.description}
       </p>
+      <button type="button" className="inline-flex items-center justify-center float-right gap-2 px-6 py-3 text-sm font-semibold text-white transition-all bg-blue-500 border border-transparent rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 m-2.5">
+  DELETE
+</button>
   </div>
 </div>
   )
